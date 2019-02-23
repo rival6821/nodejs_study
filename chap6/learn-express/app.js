3e3ec0a63c9d3ca9ac6c36dc6cccf44166179d4a
ilhoon lee <rival6821@naver.com>
2019-02-23 21:25:55 +0900

    세션관리 미들웨어 추가

--- chap6/learn-express/app.js
+++ chap6/learn-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,11 +15,34 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// middleware test
+app.use(function(req,res,next){
+  console.log(req.url, '미들웨어입니다');
+  next();
+});
+
 app.use(logger('dev'));
+
+// 정적 파일 위치 저장
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('secret code'));
+
+// 세션관리 미들웨어
+app.use(session({
+  resave:false,
+  saveUninitialized : false,
+  secret : 'secret code',
+  cookie : {
+    httpOnly : true,
+    secure : false,
+  }
+}));
+
+// 일회성 메세지 날림
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
