99f02729b67834a0e98066ad5b6ed55f7e0274e7
ilhoon lee <rival6821@naver.com>
2019-03-13 21:14:31 +0900

    유저 라우터 까지 작업 338p

--- chap8/learn-mongoose/routes/index.js
+++ chap8/learn-mongoose/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../schemas/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.find({})
+    .then((users)=>{
+      res.render('mongoose',{users});
+    })
+    .catch((err)=>{
+      console.error(err);
+      next(err);
+    });
 });
 
 module.exports = router;
--- chap8/learn-mongoose/schemas/index.js
+++ chap8/learn-mongoose/schemas/index.js
@@ -2,17 +2,27 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
     const connect = () => {
-        if(precess.env.NODE_ENV !== 'production'){
+        if(process.env.NODE_ENV !== 'production'){
             mongoose.set('debug',true);
         }
-        mongoose.connect('mongoose://root:aa8810@localhost:27017/admin',{
+        mongoose.connect('mongodb://root:aa8810@localhost:27017/admin',{
             dbName : 'nodejs',
         }, (error) => {
             if(error){
                 console.log('몽고디비 연결 에러',error);
             }else{
-                console.log('몽고디비 연결 성공')
+                console.log('몽고디비 연결 성공');
             }
-        })
-    }
+        });
+    };
+    connect();
+    mongoose.connection.on('error',(error)=>{
+        console.log('몽고디비 연결 에러',error);
+    });
+    mongoose.connection.on('disconnected',(error)=>{
+        console.log('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다');
+        connect();
+    });
+    require('./user');
+    require('./comment');
 }
