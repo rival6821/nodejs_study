4c6ad208336ebef9a99c791ba81f204673d4150e
ilhoon <rival6821@naver.com>
2019-04-20 17:24:28 +0900

    아 에러 뜬다;;; 모르겟다

--- chap10/nodebird-api/passport/index.js
+++ chap10/nodebird-api/passport/index.js
@@ -9,19 +9,19 @@ module.exports = (passport) => {
 
   passport.deserializeUser((id, done) => {
     User.findOne({
-      where : {id},
-      include : [{
-        model : User,
-        attributes : ['id','nick'],
-        as : 'Followers',
-      },{
-        model : User,
-        attributes : ['id','nick'],
-        as : 'Followings',
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
       }],
     })
-    .then(user => done(null,user))
-    .catch(err=>done(err));
+      .then(user => done(null, user))
+      .catch(err => done(err));
   });
 
   local(passport);
